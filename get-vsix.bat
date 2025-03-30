51216b6f6ab1445823a9e5b80e9ac040d7a44088
smzht <smzhts1@gmail.com>
2025-03-30 15:36:32 +0900

    README を作成したので、コマンド内のコメントを整理

--- get-vsix.bat
+++ get-vsix.bat
@@ -1,17 +1,5 @@
 @echo off
 
-REM 本バッチファイルは、shellスクリプト get-vsix を Microsoft Copilot に変換してもらったものを
-REM ベースに作成しました
-REM 最後に実行していた wget は、Windows に標準インストールされている curl に置き換えています
-
-REM https://www.reddit.com/r/vscode/comments/1i6k7gf/cant_download_vsix_extensions_from_the_web/
-
-REM <Examples>
-REM  get-vsix MS-CEINTL.vscode-language-pack-ja
-REM  get-vsix MS-CEINTL.vscode-language-pack-ja latest
-REM  get-vsix MS-CEINTL.vscode-language-pack-ja 1.99.2025032609
-REM  get-vsix MS-CEINTL.vscode-language-pack-ja 1.98.2025031209
-
 REM コマンド名の取得
 set "command_name=%~n0"
 
@@ -46,7 +34,6 @@ if "%part2%"=="" (
     goto usage_exit
 )
 
-REM wgetコマンドの実行
+REM curlコマンドの実行
 set "url=https://%part1%.gallery.vsassets.io/_apis/public/gallery/publisher/%part1%/extension/%part2%/%version%/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
-REM wget -O "%part1%.%part2%-%version%.vsix" "%url%"
 curl -L --ssl-no-revoke "%url%" -o "%part1%.%part2%-%version%.vsix"
